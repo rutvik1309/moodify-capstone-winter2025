e5c32c1b3b263a2ac41ddb8a9a3e9be1136fb1cc
Rutvik <rutvikpatel3948@gmail.com>
2025-04-13 16:52:38 -0400

    Fix

--- backend-final/server.js
+++ backend-final/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const playlistRoutes = require('./routes/playlistRoutes');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
